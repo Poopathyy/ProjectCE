8205f938dd7ded00c6a58cf1310c87088c92b962
Poopathy <poopathyswaq@gmail.com>
2026-01-12 12:44:34 +0800

    Update ga_scheduler.py

--- ga_scheduler.py
+++ ga_scheduler.py
@@ -1,26 +1,23 @@
 import random
-from fitness import fitness_function
-from utils import random_assignment
+from src.utils import random_assignment, generate_timeslots
+from src.fitness import fitness_function
 
 def selection(population, fitnesses):
     return random.choices(population, weights=fitnesses, k=2)
 
-def crossover(parent1, parent2):
-    point = random.randint(1, len(parent1)-1)
-    return parent1[:point] + parent2[point:]
+def crossover(p1, p2):
+    point = random.randint(1, len(p1) - 1)
+    return p1[:point] + p2[point:]
 
 def mutation(chromosome, timeslots, rooms, rate=0.1):
-    for i in range(len(chromosome)):
+    for gene in chromosome:
         if random.random() < rate:
-            exam, _, _ = chromosome[i]
-            chromosome[i] = (
-                exam,
-                random.choice(timeslots),
-                random.choice(rooms)
-            )
+            gene["timeslot"] = random.choice(timeslots)
+            gene["room"] = random.choice(rooms)
     return chromosome
 
-def run_ga(exams, rooms, timeslots, generations=200, pop_size=50):
+def run_ga(exams, rooms, generations=200, pop_size=50):
+    timeslots = generate_timeslots()
     population = [
         random_assignment(exams, rooms, timeslots)
         for _ in range(pop_size)
@@ -30,29 +27,22 @@ def run_ga(exams, rooms, timeslots, generations=200, pop_size=50):
     best_fitness = 0
     history = []
 
-    for gen in range(generations):
-        fitnesses = [
-            fitness_function(ind, exams, rooms)
-            for ind in population
-        ]
-
+    for _ in range(generations):
+        fitnesses = [fitness_function(ind, rooms) for ind in population]
         history.append(max(fitnesses))
 
         new_population = []
         for _ in range(pop_size):
             p1, p2 = selection(population, fitnesses)
             child = crossover(p1, p2)
-            child = mutation(
-                child,
-                timeslots,
-                rooms['room_id'].tolist()
-            )
+            child = mutation(child, timeslots, rooms["room_id"].tolist())
             new_population.append(child)
 
         population = new_population
 
-        if max(fitnesses) > best_fitness:
-            best_fitness = max(fitnesses)
-            best_solution = population[fitnesses.index(best_fitness)]
+        max_fit = max(fitnesses)
+        if max_fit > best_fitness:
+            best_fitness = max_fit
+            best_solution = population[fitnesses.index(max_fit)]
 
     return best_solution, history
